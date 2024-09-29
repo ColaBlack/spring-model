20b8ebd12f01f76687469ec0ecd4f5d6888cbc8f
ColaBlack <2698360477@qq.com>
2024-09-29 20:26:47 +0800

    initital commit

---
+++ src/main/java/edu/zafu/teaai/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package edu.zafu.teaai.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.zafu.teaai.model.po.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author ColaBlack
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
